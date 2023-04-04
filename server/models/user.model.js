055e6ccccc06e1506523fa72d07eb7c9f7c8ac0c
Matvey <matvej.duriagin@yandex.ru>
2023-04-04 17:27:02 +0300

    Add connection to DB and creating Admin

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const User = mongoose.model("User", new mongoose.Schema({
     username: String,
     email: String,
-    email: String,
+    password: String,
 }))
 
 module.exports = User;
