32c65390a393643eab1282c33014e3131420a984
Matvey <matvej.duriagin@yandex.ru>
2023-04-04 17:36:38 +0300

    Add connection to DB and creating Admin

--- server/models/index.js
+++ server/models/index.js
@@ -2,9 +2,10 @@ const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
 const db = {};
+db.mongoose = mongoose;
 
 db.user = require("./user.model");
 db.chat = require("./chat.model");
-db.message = require("./chat.model");
+db.message = require("./message.model");
 
 module.exports = db;
